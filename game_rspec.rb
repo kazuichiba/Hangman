4c053ee7219a05b960547531eec7aa5f6a4f8712
Kazuki Ichiba <Kazu.Ichiba@Gmail.com>
2013-06-03 19:14:38 -0400

    specified number of players for our game

--- game_rspec.rb
+++ game_rspec.rb
@@ -3,12 +3,25 @@ require_relative 'game'
 
 describe Game do
   describe "the number of players" do
-    it "accepts an integer amount of players" do
+    it "allows the amount of players to be set" do
       game = Game.new(1)
       expect(game.player_count).to eql(1)
     end
 
-    it "allows up to five players"
+    describe "allows up to five players" do
+      it "should allow five players" do
+        game = Game.new(5)
+        expect(game.has_valid_player_count?).to be_true
+      end
 
+      it "shouldnt allow 6 players" do
+        game = Game.new(6)
+        expect(game.has_valid_player_count?).to be_false
+      end
+      it "shouldnt allow zero players" do
+        game = Game.new(0)
+        expect(game.has_valid_player_count?).to be_false
+      end
+    end
   end
 end
